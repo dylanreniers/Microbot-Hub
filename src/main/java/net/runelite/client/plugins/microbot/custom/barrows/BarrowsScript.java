860121fc591254ec348b5758e12a552fe12295e3
Dylan Reniers <dylan.reniers@digitalum.eu>
2026-01-04 20:59:31 +0100

    Refactoring

--- src/main/java/net/runelite/client/plugins/microbot/custom/barrows/BarrowsScript.java
+++ src/main/java/net/runelite/client/plugins/microbot/custom/barrows/BarrowsScript.java
@@ -580,8 +580,8 @@ public class BarrowsScript extends Script {
     }
 
     private void activatePrayer(BarrowsBrother brother) {
-        if (Objects.nonNull(brother) && !Rs2Prayer.isPrayerActive(brother.whatToPray)) {
-            Rs2Prayer.toggle(brother.whatToPray);
+        if (Objects.nonNull(brother) && !Rs2Prayer.isPrayerActive(brother.getWhatToPray())) {
+            Rs2Prayer.toggle(brother.getWhatToPray());
         }
     }
 
@@ -773,73 +773,6 @@ public class BarrowsScript extends Script {
         if (piece != null) {
             barrowsPieces.add(piece.getName());
             barrowsPieces.remove("Nothing yet.");
-        }
-    }
-
-    @Getter
-    public enum BarrowsBrother {
-        //Note: this is the order in which the brothers are killed.
-        DHAROK("Dharok the Wretched", new Rs2WorldArea(3573, 3296, 3, 3, 0), Rs2PrayerEnum.PROTECT_MELEE, VarbitID.BARROWS_KILLED_DHAROK, true),
-        KARIL("Karil the Tainted", new Rs2WorldArea(3564, 3274, 3, 3, 0), Rs2PrayerEnum.PROTECT_RANGE, VarbitID.BARROWS_KILLED_KARIL, true),
-        AHRIM("Ahrim the Blighted", new Rs2WorldArea(3563, 3288, 3, 3, 0), Rs2PrayerEnum.PROTECT_MAGIC, VarbitID.BARROWS_KILLED_AHRIM, false),
-        GUTHAN("Guthan the Infested", new Rs2WorldArea(3575, 3280, 3, 3, 0), Rs2PrayerEnum.PROTECT_MELEE, VarbitID.BARROWS_KILLED_GUTHAN, true),
-        TORAG("Torag the Corrupted", new Rs2WorldArea(3552, 3282, 2, 2, 0), Rs2PrayerEnum.PROTECT_MELEE, VarbitID.BARROWS_KILLED_TORAG, true),
-        VERAC("Verac the Defiled", new Rs2WorldArea(3556, 3297, 3, 3, 0), Rs2PrayerEnum.PROTECT_MELEE, VarbitID.BARROWS_KILLED_VERAC, true);
-
-        private final String name;
-        private final Rs2WorldArea moundArea;
-        private final Rs2PrayerEnum whatToPray;
-        private final int varbit;
-        private final boolean needsMeleeGear;
-
-        BarrowsBrother(String name, Rs2WorldArea moundArea, Rs2PrayerEnum whatToPray, int varbit, boolean needsMeleeGear) {
-            this.name = name;
-            this.moundArea = moundArea;
-            this.whatToPray = whatToPray;
-            this.varbit = varbit;
-            this.needsMeleeGear = needsMeleeGear;
-        }
-
-        public static BarrowsBrother getFirstBarrowsBrother() {
-            return BarrowsBrother.values()[0];
-        }
-
-        public static boolean allBarrowsBrothersAreKilled() {
-            return Arrays.stream(BarrowsBrother.values()).filter(BarrowsBrother::hasBeenKilled).count() == 6;
-        }
-
-        public static int getNumberOfBarrowsBrothersKilled() {
-            return (int) Arrays.stream(BarrowsBrother.values()).filter(BarrowsBrother::hasBeenKilled).count();
-        }
-
-        public static BarrowsBrother getFinalBarrowsBrother() {
-            return Arrays.stream(BarrowsBrother.values()).filter((brother) -> !brother.hasBeenKilled()).findFirst().orElse(null);
-        }
-
-        public boolean hasBeenKilled() {
-            return Microbot.getVarbitValue(this.varbit) == 1;
-        }
-
-        public boolean isDharok() {
-            return this.name.startsWith("Dharok");
-        }
-
-        public boolean isAhrim() {
-            return this.name.startsWith("Ahrim");
-        }
-
-        public boolean isKaril() {
-            return this.name.startsWith("Karil");
-        }
-
-        public boolean isWeakerBrother() {
-            return !isDharok() && !isAhrim() && !isKaril();
-        }
-    }
-
-    public static class BarrowsScriptException extends RuntimeException {
-        public BarrowsScriptException(String message) {
-            super(message);
         }
     }
 
